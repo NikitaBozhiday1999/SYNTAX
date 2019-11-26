3f8ef47c26516eaff68268cde514a88fd80271b3
Nikita <nikitaboz19@gmail.com>
2019-11-26 18:27:24 +0300

    прошел уро 19

---
+++ lesson-19-cw/index.js
@@ -0,0 +1,32 @@
+// prompt('Введите ваш возраст', 18);
+// // confirm('Вы уверены что хотите свалить?');
+// // let age = '35';
+// let age = prompt('Введите ваш возраст');
+
+// // alert(age);
+// console.log('Возраст Никиты', age);
+// const message = 'Hello world';
+// message = 'Bye';
+// let sentense;
+
+// let isAdmin;
+
+// let name = null;
+
+// name = 'Jack';
+
+// let age = prompt('Ваш возраст:');
+
+// console.log(typeof age);
+
+// alert('hello');
+// prompt('enter name', 'Nick')
+
+let name = prompt('Введите имя');
+
+alert(name);
+
+console.log (name);
+
+let gick;
+console.log(gick);
