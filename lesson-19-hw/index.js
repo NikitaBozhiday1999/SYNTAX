59e1a5d89104e352e5046d3c49139fa9c473ce4b
Nikita <nikitaboz19@gmail.com>
2019-11-26 21:39:44 +0300

    Работал над домашкой 19 урока

---
+++ lesson-19-hw/index.js
@@ -0,0 +1,119 @@
+// Переменные
+// Задание 1
+let admin,
+    name;
+
+name = 'Джон';
+admin = 'Джон';
+
+alert(admin);
+
+// Задание 2
+let BigPlanetName = 'Earth';
+let userName = 'Nick';
+
+// Задание 3
+const BIRTHDAY;
+const age;
+
+// Типы данных
+// Задание 1
+let name = 'Ilya';
+alert(`hello ${1}`); // hello 1
+alert(`hello ${'name'}`); // hello name
+alert(`hello ${name}`); // hello Ilya
+
+// Преобразование типов
+// Задание 1
+'' + 1 + 0 = '10';
+'' - 1 + 0 = -1;
+true + false = 1;
+6 / '3' = 2;
+'2' * '3' = 6;
+4 + 5 + 'px' = '9px';
+'$' + 4 + 5 = '$45';
+'4' - 2 = 2;
+'4px' - 2 = NaN;
+7 / 0 = Infinity;
+'  -9  ' + 5 = '  -9  5';
+'  -9  ' - 5 = -14;
+null + 1 = 1;
+undefined + 1 = NaN;
+' \t \n' - 2 = -2;
+
+// Операторы 
+// Задание 1
+a = 2;
+b = 2;
+c = 2;
+d = 1;
+// Задание 2
+a = 4;
+x = 5;
+
+// Операторы сравнения 
+// Задание 1
+5 > 4 true;
+'ананас' > 'яблоко' false;
+'2' > '12' true;
+undefined == null true;
+undefined === null false;
+null == '\n0\n' false;
+null === +'\n0\n' false;
+
+// Условные операторы
+// Задание 1
+if ('0') {
+    alert('Привет!');
+}
+да, выведется 
+// Задание 2
+let officialName = prompt('Какое "официальное название" JavaScript?');
+
+if (officialName == 'ECMAScript') {
+    alert('Верно!');
+}
+else {
+    alert('Ты не знаешь, ууу? ECMAScript!');
+}
+// Задание 3
+let numbAge = prompt('Сколько вам сахара?', 0);
+
+if (numbAge > 0) {
+    alert(1);
+}
+else if (numbAge < 0) {
+    alert(-1);
+}
+else {
+    alert(0);
+}
+
+// Задание 4
+let result = (a + b < 4) ? 'little' : 'much';
+// Задание 5
+let message = (login == 'Сотрудник') ? 'Привет':
+(login == 'Директор') ? 'Здравствуйте':
+(login == '') ? 'Нет логина' :
+'';
+
+// Логические операторы 
+// Задание 1
+2
+// Задание 2
+alert(1), 2
+// Задание 3
+0
+// Задание 4
+1, undefined
+// Задание 5
+3
+// Задание 6
+if (age >= 14 && age <= 90);
+// Задание 7
+if (age ! >=14 && <=90);
+// Задание 8
+
+// Задание 9
+
+
