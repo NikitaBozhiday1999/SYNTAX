7dd59f54e35cbeab38dbbb0c410452f660a6e2ac
Nikita <nikitaboz19@gmail.com>
2019-12-03 16:45:12 +0300

    Сделал домашку 20

--- lesson-20-hw/index.js
+++ lesson-20-hw/index.js
@@ -43,3 +43,32 @@ let num1 = +prompt('Введите первое число');
 let num2 = +prompt('Введите второе число');
 alert(num1+num2);
 
+alert( Math.round(6.35 * 10) / 10); // 6.4
+
+// Массивы
+let fruits = [1, 2, 3, 4];
+let shoppingCart = fruits;
+shoppingCart.push(55);
+alert(fruits.length);// да скопираован выведен - 4
+
+let styles = ['Джаз', 'Блюз'];
+styles.push('Рок-н-ролл');
+styles.splice(1, 1, 'Классика');
+alert(styles.shift());
+styles.unshift('Реп', 'Регги');
+
+
+// Map и Set 
+let map = new Map();
+map.set('name', 'John');
+let keys = Array.from(map.key())
+key.push('more');
+alert(key); // name, more
+
+
+// WeakMap и  weekSet
+
+
+// Дата и время 
+let now = new Date(2012, 2, 20, 3, 12);
+alert(now);
