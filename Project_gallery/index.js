cced47dfd86a7d4e6b3e16f911a419f8d9a68f74
Nikita <nikitaboz19@gmail.com>
2020-01-23 16:51:04 +0300

    попытка пагинации

--- Project_gallery/index.js
+++ Project_gallery/index.js
@@ -4,31 +4,23 @@ let elementMenu;
 let spiner = document.querySelector('.preloader-wrapper');
 const LEFT_CHEVRON_SYMBOLS = 12;
 const RIGHT_CHEVRON_SYMBOLS = 13;
+let leftArrow = document.querySelector('.arrowL');
+let rightArrow = document.querySelector('.arrowR');
 
- 
-
-function buildGallery(image) {
-  let bodyEl = document.querySelector(".row");
-  bodyEl.innerHTML = '';
-  for(let i = 0; i < image.length; i++) {
-      let rand = Math.round(100 + Math.random() * 100);
-      image[i].height = rand;
-      image[i].width = rand;
-      rand = Math.round(1 + Math.random() * 1000);
-      image[i].id = rand;
-      image[i].download_url = `https://picsum.photos/id/${image[i].id}/${image[i].height}/${image[i].width}`;
-      let cardImg = `<img class="col xl3 l4 m6 s12 materialboxed" src="${image[i].download_url}">`;
-      bodyEl.insertAdjacentHTML('beforeend', cardImg);
-      M.AutoInit();
-  }
-}
 
 
 async function readImage() {
+    let page = localStorage.getItem('value');
+    let bodyEl = document.querySelector(".row");
+    bodyEl.innerHTML = '';
     try {
-        let img = await fetch(`https://picsum.photos/v2/list?page=2&limit=10`);
+        let img = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`);
         let images = await img.json();
-        buildGallery(images);
+        for (let image of images) {
+            let cardImg = `<img class="col xl3 l4 m6 s12 materialboxed" src="${image.download_url}">`;
+            bodyEl.insertAdjacentHTML('beforeend', cardImg);
+        }
+        M.AutoInit();
     }
     catch {
         alert("So sorry, it's error...")
@@ -39,10 +31,8 @@ async function readImage() {
 }
 
 function startSession() {
-    console.log(localStorage);
     if(localStorage.length == 0) {
         localStorage.setItem('value', 1);
-        console.log(localStorage);
     }
         targetEl = listEl.children[localStorage.getItem('value')];
         targetEl.classList.add('active');
@@ -50,21 +40,33 @@ function startSession() {
 }
 
 function pagination(event) {
-    if(localStorage.getItem('value') > 5) {
-        targetEl.classList.add('disabled');
-        targetEl.classList.remove('disabled');
+    if (localStorage.getItem('value') == 5) {
+        // elementMenu = listEl.children[localStorage.getItem('value')];
+        rightArrow.classList.add('disabled');
     }
-    else if(localStorage.getItem('value') == 1) {
-        targetEl.classList.add('disabled');
-        targetEl.classList.remove('disabled');
-    }
-    else if (localStorage.getItem('value') > 1 && localStorage.getItem('value') < 5) {
-        targetEl.classList.remove('disabled');
-    }
-    targetEl = listEl.children[listEl.children.length - 1];
-    targetEl = listEl.children[0];
-    if ((localStorage.getItem('value') == 1 && event.target.textContent.length == LEFT_CHEVRON_SYMBOLS) || (localStorage.getItem('value') == 5 && event.target.textContent.length == 13)) {
-        return ;
+    // else if (localStorage.getItem('value' < 5)) {
+    //     rightArrow.classList.remove('disabled');
+    // }
+
+
+
+
+    
+    // if(localStorage.getItem('value') > 5) {
+    //     targetEl.classList.add('disabled');
+    //     targetEl.classList.remove('disabled');
+    // }
+    // else if(localStorage.getItem('value') == 1) {
+    //     targetEl.classList.add('disabled');
+    //     targetEl.classList.remove('disabled');
+    // }
+    // else if (localStorage.getItem('value') > 1 && localStorage.getItem('value') < 5) {
+    //     targetEl.classList.remove('disabled');
+    // }
+    // targetEl = listEl.children[listEl.children.length - 1];
+    // targetEl = listEl.children[0];
+    if ((localStorage.getItem('value') == 1 && event.target.textContent.length == LEFT_CHEVRON_SYMBOLS) || (localStorage.getItem('value') == 5 && event.target.textContent.length == RIGHT_CHEVRON_SYMBOLS)) {
+        return;
     }
     else if (localStorage.getItem('value') > 1 && event.target.textContent.length == LEFT_CHEVRON_SYMBOLS) {
         localStorage.setItem('value', +localStorage.getItem('value') - 1);
@@ -88,9 +90,9 @@ listEl.addEventListener('click', pagination);
 startSession();
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.materialboxed');
-    M.Materialbox.init(elems);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     let elems = document.querySelectorAll('.materialboxed');
+//     M.Materialbox.init(elems);
+//   });
 
 
